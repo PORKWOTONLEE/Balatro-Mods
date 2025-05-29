c7f7a263e8fabbd2274e6db83c8e3bbace506f5e
Lizongze <lizongze@amazingdm.com>
2025-05-29 17:32:28 +0800

    添加詹克琼克勒斯小丑模组JankJonklers-V0.22

---
+++ JankJonklers-V0.22/localization/zh_CN.lua
@@ -0,0 +1,288 @@
+return {
+	descriptions = {
+		Joker = {
+			j_jank_pawn = {
+				name = "典当小丑",
+				text = {
+					"当你{C:attention}出售{} 一张牌",
+					"{C:green}#1# / #2#{} 几率 ",
+					"获得 {C:attention}$#3#{} ",
+				},
+			},
+			j_jank_expanded_art = {
+				name = "艺术小丑",
+				text = {
+					"{C:attention}增强牌{} 计分时提供",
+					"{C:chips}+#1#{}筹码",
+				},
+			},
+			j_jank_self_portrait = {
+				name = "小丑自画像",
+				text = {
+					"获得 {X:mult,C:white}X0.1{} 倍率",
+					"whenever you {C:attention}#1#{}.",
+					"Changes every blind.",
+					"{C:inactive}(当前 {X:mult,C:white}X#2#{}{C:inactive} Mult){}",
+				},
+			},
+			j_jank_mural_menace = {
+				name = "Mural Menace",
+				text = {
+					"当你跳过 {C:attention}盲注{}",
+					"创建一个随机的 {C:attention}标签{}",
+				},
+			},
+			j_jank_minimalist = {
+				name = "极简主义小丑",
+				text = {
+					"如果打出的牌不包含 {C:attention}人头牌{}",
+					"{C:mult}+#1#{} 倍率",
+				},
+			},
+			j_jank_devoted = {
+				name = "奉献小丑",
+				text = {
+					"选择 {C:attention}BOSS盲注{} 后",
+					"增加 {X:mult,C:white}X#2#{} 倍率",
+					"将你的 {C:attention}金币{} 变为 {C:attention}$0{}",
+					"{C:inactive}(当前 {X:mult,C:white} X#1# {C:inactive})",
+				},
+			},
+			j_jank_ternary_system = {
+				name = "三三星球",
+				text = {
+					"如果打出的牌包含 {C:attention}3{}",
+					"并且是 {C:attention}三条{}",
+					"创建一个 {C:planet}星球牌{} ",
+					"{C:inactive}(必须有空间)",
+				},
+			},
+			j_jank_stanczyk = {
+				name = "斯坦兹克",
+				text = {
+					"重新触发打出或手牌中的 {C:attention}增强牌{}",
+				},
+			},
+			j_jank_cut_the_cheese = {
+				name = "切奶酪",
+				text = {
+					"当选择 {C:attention}盲注{} 后",
+					"创建一个随机的{C:attention}食物小丑{}",
+					"{C:inactive}(必须有空间){}",
+				},
+			},
+			j_jank_boredom_slayer = {
+				name = "无聊杀手",
+				text = {
+					"减少 {C:attention}盲注{} {C:attention}#1#%{} 要求",
+				},
+			},
+			j_jank_fortuno = {
+				name = "Fortuno",
+				text = {
+					"每回合第一次出牌时",
+					"每张计分的{C:attention}数字牌{} ",
+					"摧毁并获得 {C:attention}$#1#{}",
+				},
+			},
+			j_jank_chalk_outline = {
+				name = "粉笔轮廓",
+				text = {
+					"这回合 {C:attention}最后一次出牌{}",
+					"这张牌获得 {C:mult}+6{} 倍率",
+					"{C:inactive}(当前 {C:mult}+#1#{C:inactive} 倍率){}",
+				},
+			},
+			j_jank_memorable = {
+				name = "难忘小丑",
+				text = {
+					"每连续打出一手牌",
+					"其中正好有 {C:attention}3{} 张牌",
+					"这张牌获得 {X:mult,C:white} X#2# {} 倍率",
+					"{C:inactive}(当前 {X:mult,C:white}X#1#{}{C:inactive} 倍率){}",
+				},
+			},
+			j_jank_scrapper = {
+				name = "刮刀小丑",
+				text = {
+					"当你弃掉五张 {C:attention}数字牌{} ",
+					"{C:green}#1# / #2#{} 几率获得一个随机的{C:attention}星球牌{}",
+				},
+			},
+			j_jank_lieutenant = {
+				name = "上尉小丑",
+				text = {
+					"打出 {C:attention}高牌{}后",
+					"每张打出的牌",
+					"增强牌型 {C:attention}等级{}",
+				},
+			},
+			j_jank_sunday_funnies = {
+				name = "周日趣事",
+				text = {
+					"再商店中每重掷两次",
+					"获得一张随机的 {C:tarot}塔罗牌{}或者星球牌",
+					"{C:inactive}(剩余{C:green}#1#{}{C:inactive} 次重掷){}",
+					"{C:inactive}(必须有空间){}",
+				},
+			},
+			j_jank_box_of_stuff = {
+				name = "一箱东西",
+				text = {
+					"选择 {C:attention}Boss盲注{} 之后",
+					"创建三个免费 {C:attention} 标签{},",
+					"然后这张牌自毁",
+				},
+			},
+			j_jank_sentai = {
+				name = "森台小丑",
+				text = {
+					"每使用一张{C:attention}星球牌{} ",
+					"获得 {C:mult}+#2#{}倍率",
+					"{C:attention}Boss 盲注{} 击败后重置",
+					"{C:inactive}(当前 {C:mult}+#1#{C:inactive} 倍率)",
+				},
+			},
+			j_jank_old_man = {
+				name = "老丑",
+				text = {
+					"选择 {C:attention}Boss 盲注{} 之后",
+					"创建一个 {C:attention}永恒标签 {}",
+				},
+			},
+			j_jank_cardslinger = {
+				name = "卡片机",
+				text = {
+					"当每张牌计分时",
+					"{C:chips}+#1#{} 筹码",
+				},
+			},
+			j_jank_shady_dealer = {
+				name = "黑心商人",
+				text = {
+					"卖掉这张牌获得",
+					"一个 {C:attention}负片标签{}",
+				},
+			},
+			j_jank_feste = {
+				name = "Feste",
+				text = {
+					"再 {C:attention}Boss 盲注{}时第一次出牌的牌型",
+					"升 {C:attention}#1#{} 级",
+				},
+			},
+			j_jank_chicken_scratch = {
+				name = "Chicken Scratch",
+				text = {
+					"如果计分的牌包含",
+					" {C:attention}8{}, {C:attention}7{}, 或 {C:attention}3{}",
+					"这张牌获得 {C:chips}+#2#{} 筹码",
+					"{C:inactive}(当前{C:chips}+#1#{C:inactive} 筹码){}",
+				},
+			},
+			j_jank_devilish = {
+				name = "恶魔小丑",
+				text = {
+					"如果打出的牌只有{C:attention}6{}或 {C:attention}黄金牌{}",
+					"{X:mult,C:white} X#1# {} 倍率 ",
+				},
+			},
+			j_jank_pitiful = {
+				name = "可悲小丑",
+				text = {
+					"如果打出的是{C:attention}高牌{} 或 {C:attention}对子{}",
+					"{C:mult}+#1#{} 倍率",
+				},
+			},
+			j_jank_tapestry = {
+				name = "挂毯小丑",
+				text = {
+					"没使用一张不同的消耗牌",
+					"这张牌获得 {C:mult}+#2#{} 倍率",
+					"{C:inactive}(当前 {C:mult}+#1#{}{C:inactive} 倍率){}",
+				},
+			},
+			j_jank_wanted_poster = {
+				name = "通缉海报",
+				text = {
+					"当你第一次出牌击败盲注",
+					"获得 {C:attention}$#1#{} ",
+					"否则失去 {C:attention}$#2#{} ",
+				},
+			},
+			j_jank_midnight_crew = {
+				name = "午夜剧团",
+				text = {
+					"如果计分牌是包含 {V:1}#1#{}的",
+					"{C:attention}同化 {}",
+					"这张牌增加 {X:mult,C:white} X0.5 {} 倍率",
+					"每回合改变",
+					"{C:inactive}(当前 {X:mult,C:white}X#2#{C:inactive})",
+				},
+			},
+			j_jank_makeshift = {
+				name = "临时小丑",
+				text = {
+					"This Joker gains {C:mult}+#2#{} Mult",
+					"per {C:attention}card{} sold",
+					"{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)",
+				},
+			},
+			j_jank_sir = {
+				name = "小丑先生",
+				text = {
+					"{C:attention}Boss 盲注{} 期间",
+					"或者最后一次 {C:attention} 出牌{}",
+					"{X:mult,C:white} X#1# {} 倍率 ",
+				},
+			},
+			j_jank_suspicious_vase = {
+				name = "可疑花瓶",
+				text = {
+					"所有 {C:attention}2{}, {C:attention}3{} and {C:attention}4{}",
+					"变为 {C:attention}玻璃牌{} 并且",
+					"打出时提供 {X:mult,C:white} X#1# {} 倍率",
+				},
+			},
+			j_jank_impractical = {
+				name = "不切实际小丑",
+				text = {
+					"如果打出的牌型是{C:attention}#1#{},",
+					" {X:mult,C:white} X#2# {} 倍率",
+					"每次触发后会改变",
+				},
+			},
+			j_jank_highlander = {
+				name = "高原小丑",
+				text = {
+					"打出{C:attention}高牌{}时",
+					"每张计分的牌永久",
+					"增加 {C:chips}+#1#{} 筹码",
+				},
+			},
+			j_jank_jevil = {
+				name = "杰维尔",
+                text = {
+                    "{C:attention}同花顺{} 计分时提供",
+                    "{C:chips}+1{} 筹码 "
+                }
+			}
+		},
+	},
+	misc = {
+		challenge_names = {
+			c_jankch_pawnshop = "当铺",
+			c_jankch_LoJ = "传奇金宝",
+		},
+		dictionary = {
+			self_portrait_1 = "使用一张塔罗牌",
+            self_portrait_2 = "使用一张星球牌",
+            self_portrait_3 = "摧毁一张牌",
+            self_portrait_4 = "打出三张牌或少于三张牌",
+            self_portrait_5 = "丢弃三或以上张人头牌",
+            self_portrait_6 = "打出高牌",
+            self_portrait_7 = "打出顺子",
+            self_portrait_8 = "打出同化",
+		}
+	}
+}
